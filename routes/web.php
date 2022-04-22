e6a3c085c1611780ba7d371dea1d0c17bed84b33
luochun <luochun15@gmail.com>
2022-04-22 19:53:39 +0800

    create session

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,6 @@
 <?php
+
+use App\Http\Controllers\SessionsController;
 use App\Http\Controllers\StaticPagesController;
 use App\Http\Controllers\UsersController;
 use Illuminate\Support\Facades\Route;
@@ -19,3 +21,6 @@ Route::get('/help',[StaticPagesController::class,'help'])->name('help');
 Route::get('/about',[StaticPagesController::class,'about'])->name('about');
 Route::get('/signup', [UsersController::class,'create'])->name('signup');
 Route::resource('/users',UsersController::class);
+Route::get('login',[SessionsController::class,'create'])->name('login');
+Route::post('login',[SessionsController::class,'store'])->name('login');
+Route::get('logout',[SessionsController::class,'destory'])->name('logout');
