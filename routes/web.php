04333a0bbba877478ae35fb0299f73d495b7dccf
luochun <luochun15@gmail.com>
2022-04-22 16:51:48 +0800

    usershow page

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 use App\Http\Controllers\StaticPagesController;
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\UsersController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,4 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/',[StaticPagesController::class,'home'])->name('home');
 Route::get('/help',[StaticPagesController::class,'help'])->name('help');
 Route::get('/about',[StaticPagesController::class,'about'])->name('about');
-Route::get('signup', [UserController::class,'create'])->name('signup');
+Route::get('/signup', [UsesrController::class,'create'])->name('signup');
+Route::resource('/users',UsersController::class);
