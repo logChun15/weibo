32bb27aee6347ac0b42698d8a17ec1fd2b05b3c1
luochun <luochun15@gmail.com>
2022-04-22 17:53:52 +0800

    complete user signup function

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,16 +1,19 @@
 <!DOCTYPE html>
 <html>
- <head>
- <title>@yield('title', 'Weibo App') - Laravel 入门教程</title>
- <link rel="stylesheet" href="{{ mix('css/app.css') }}">  {{-- mix用于解决浏览器缓存 --}}
- </head>
- <body>
+<head>
+        <title>@yield('title', 'Weibo App') - Laravel 入门教程</title>
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">  {{-- mix用于解决浏览器缓存 --}}
+</head>
+
+<body>
  @include('layouts._header')
- <div class="container">
- <div class="offset-md-1 col-md-10">
- @yield('content')
- @include('layouts._footer')
- </div>
- </div>
- </body>
+   <div class="container">
+        <div class="offset-md-1 col-md-10">
+            @include('shared._messages')
+            @yield('content')
+            @include('layouts._footer')
+        </div>
+   </div>
+
+</body>
 </html>
