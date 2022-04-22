50673160c20162a57b6c1843a65b7b837e5aa8f1
luochun <luochun15@gmail.com>
2022-04-22 13:45:53 +0800

    user signup page

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 use App\Http\Controllers\StaticPagesController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/',[StaticPagesController::class,'home'])->name('home');
 Route::get('/help',[StaticPagesController::class,'help'])->name('help');
 Route::get('/about',[StaticPagesController::class,'about'])->name('about');
+Route::get('signup', [UserController::class,'create'])->name('signup');
