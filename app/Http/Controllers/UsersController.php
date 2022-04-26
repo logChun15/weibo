23b1d2dbef29b0a1d0dbe6dc26e5e42e9f963d5a
luochun <luochun15@gmail.com>
2022-04-26 16:58:21 +0800

    weibo data model

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -22,11 +22,16 @@ class UsersController extends Controller
     public function create()
     {
         return view('users.create');
+        
     }
 
     public function show(User $user)
     {
         return view('users.show', compact('user'));
+        $statuses = $user->statuses()
+        ->orderBy('created_at', 'desc')
+        ->paginate(10);
+        return view('users.show', compact('user', 'statuses'));
     }
 
     public function store(Request $request)
