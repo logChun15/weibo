6c617e6e5c3566ff685bc65a00e5fe8c85860c0e
luochun <luochun15@gmail.com>
2022-04-27 11:35:57 +0800

    social statistics

--- app/Models/User.php
+++ app/Models/User.php
@@ -83,7 +83,7 @@ class User extends Authenticatable
         if(! is_array($user_ids) ) {
             $user_ids=compact('user_ids');
         }
-        $this->following()->sync($user_ids,false);
+        $this->followings()->sync($user_ids,false);
     }
 
     public function unfollow($user_ids)
